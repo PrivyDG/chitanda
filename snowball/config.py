c7a82e4435198d874ec0c134e5e83704f9ecd9f5
dazzler <dazzler@riseup.net>
2019-07-09 18:47:06 -0400

    add config module

---
+++ snowball/config.py
@@ -0,0 +1,42 @@
+import json
+import logging
+import sys
+
+from snowball.constants import CONFIG_PATH
+
+logger = logging.getLogger(__name__)
+
+BLANK_CONFIG = {
+    'trigger_character': '!',
+    'irc_servers': {},
+    'discord_token': '',
+    'modules_enabled': [],
+    'admins': {},
+}
+
+
+class Config:
+    def __init__(self):
+        self.config = self.load_config()
+
+    def __getitem__(self, key):
+        try:
+            return self.config[key]
+        except TypeError:
+            logger.critical('Configuration dictionary missing, exiting.')
+            sys.exit(1)
+
+    def reload(self):
+        self.config = self.load_config()
+
+    def load_config(self):
+        if not CONFIG_PATH.exists():
+            return {}
+        with open(CONFIG_PATH, 'r') as cf:
+            try:
+                return json.load(cf)
+            except json.decoder.JSONDecodeError:
+                logger.critical('Config is not valid JSON.')
+
+
+config = Config()
