e6c663f6ae82336e048d3054fe8271e8726309ee
dazzler <dazzler@riseup.net>
2019-07-09 18:51:51 -0400

    fix permissions on data directory

--- snowball/constants.py
+++ snowball/constants.py
@@ -13,7 +13,7 @@ CONFIG_PATH = DATA_DIR / 'config.json'
 
 if not DATA_DIR.is_dir():
     try:
-        DATA_DIR.mkdir(mode=0o755)
+        DATA_DIR.mkdir(mode=0o700)
     except OSError:
         logger.critical('Could not create data directory.')
         sys.exit(1)
