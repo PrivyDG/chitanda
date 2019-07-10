38f1ec0925e23bbb38b9f2ab658a39a0dec1511e
dazzler <dazzler@riseup.net>
2019-07-10 15:34:00 -0400

    add database migration system

--- snowball/__main__.py
+++ snowball/__main__.py
@@ -3,6 +3,7 @@ import json
 
 import click
 
+import snowball.database  # noqa
 from snowball import CONFIG_PATH, cmdgroup
 from snowball.bot import Snowball
 from snowball.config import BLANK_CONFIG
@@ -25,12 +26,6 @@ def config():
         with open(CONFIG_PATH, 'w') as f:
             json.dump(BLANK_CONFIG, f, indent=4)
     click.edit(filename=CONFIG_PATH)
-
-
-@cmdgroup.command()
-def migrate():
-    """Upgrade the database to the latest migration."""
-    pass
 
 
 cmdgroup()
--- snowball/database.py
+++ snowball/database.py
@@ -1,7 +1,18 @@
+import logging
 import sqlite3
+import sys
+import time
+from collections import namedtuple
 from contextlib import contextmanager
+from pathlib import Path
 
-from snowball import DATABASE_PATH
+import click
+
+from snowball import DATABASE_PATH, cmdgroup
+
+logger = logging.getLogger(__name__)
+
+Migration = namedtuple('Migration', 'path, version, source')
 
 
 @contextmanager
@@ -11,3 +22,91 @@ def database():
         cursor = conn.cursor()
         yield conn, cursor
         cursor.close()
+
+
+@cmdgroup.command()
+def migrate():
+    """Upgrade the database to the latest migration."""
+    migrations_needed = _calculate_migrations_needed()
+
+    if not migrations_needed:
+        click.echo('Database is up to date.')
+        exit()
+
+    logger.info('Pending database migrations found.')
+    with database() as (conn, cursor):
+        for mig in migrations_needed:
+            logger.info(f'Executing migration at {mig.path} .')
+            with mig.path.open() as sql:
+                cursor.executescript(sql.read())
+                cursor.execute(
+                    'INSERT INTO versions (source, version) VALUES (?, ?)',
+                    (mig.source, mig.version)
+                )
+            conn.commit()
+
+
+def _calculate_migrations_needed():
+    migrations = _find_migrations()
+    versions = _get_versions()
+
+    needed = []
+    for mig in migrations:
+        if mig.version > versions.get(mig.source, 0):
+            needed.append(mig)
+    return sorted(needed, key=lambda m: m.version)
+
+
+def _find_migrations():
+    migrations = []
+    commands_path = Path(__file__).parent / 'commands'
+    for sql_path in commands_path.glob('**/*.sql'):
+        try:
+            migrations.append(
+                Migration(
+                    path=sql_path,
+                    version=int(sql_path.stem),
+                    source=sql_path.parts[-3],
+                )
+            )
+        except TypeError:
+            click.echo(f'Invalid migration name: {sql_path}.')
+            raise click.Abort
+    return migrations
+
+
+def _get_versions():
+    with database() as (conn, cursor):
+        cursor.execute(
+            'SELECT source, MAX(version) FROM versions GROUP BY source'
+        )
+        return {r['source']: r[1] for r in cursor.fetchall()}
+
+
+with database() as (conn, cursor):
+    cursor.execute(
+        """
+        SELECT name FROM sqlite_master
+        WHERE type='table' AND name='versions'
+        """
+    )
+    if not cursor.fetchone():
+        logger.info('Creating versions table.')
+        cursor.execute(
+            """
+            CREATE TABLE versions (
+                source TEXT,
+                version INT,
+                PRIMARY KEY (source, version)
+            )
+            """
+        )
+        conn.commit()
+
+
+if _calculate_migrations_needed():
+    if not len(sys.argv) == 2 or sys.argv[1] != 'migrate':
+        logger.warning('The database needs to be migrated.')
+        logger.warning('Run `snowball migrate`.')
+        logger.info('Sleeping for 3 seconds.')
+        time.sleep(3)
